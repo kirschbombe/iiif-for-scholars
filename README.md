a3c930bd3f46f19c8b5b3e93d208d24b6f1fb070
Dawn Childress <kirschbombe@gmail.com>
2019-05-06 10:28:30 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# IIIF Workshop @ UCLA
+
+Slides: https://docs.google.com/presentation/d/1Zwl4WrGEQybS_bq2oFpjHdfAuc66fNjbVxFlyjX8meM/edit#slide=id.g48a2113b32_0_167
+
+## I. Getting Started
---
+++ leaflet/leaflet.js
@@ -0,0 +1,33 @@
+var map;
+
+map = L.map('map', {
+  center: [0, 0],
+  crs: L.CRS.Simple,
+  zoom: 0,
+});
+
+// use this method for two requests of the same image with different parameters
+var iiifUrl = 'https://libimages.princeton.edu/loris/pudl0033/2007/04003/00000001.jp2/info.json';
+
+var layer1 = L.tileLayer.iiif(iiifUrl, {
+    fitBounds: false,
+  }
+).addTo(map);
+
+var layer2 = L.tileLayer.iiif(iiifUrl, {
+    quality: 'gray'
+  }
+).addTo(map);
+
+
+// use this method to layer two separate image requestsinfo.json
+// var layer1 = L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_natural_color_panorama_WIP2/info.json', {
+//     fitBounds: false,
+//   }
+// ).addTo(map);
+
+// var layer2 = L.tileLayer.iiif(
+//   'https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_combined_processes_WIP2_panorama_with_blue_water/info.json'
+// ).addTo(map);
+
+L.control.sideBySide(layer1, layer2).addTo(map);
