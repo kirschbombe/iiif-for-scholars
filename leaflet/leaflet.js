f782ba8c1227be7ccbbf29f00700536897a5d4e3
Dawn Childress <kirschbombe@users.noreply.github.com>
2019-05-08 12:09:24 -0700

    Update leaflet.js

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -7,27 +7,27 @@ map = L.map('map', {
 });
 
 // use this method for two requests of the same image with different parameters
-var iiifUrl = 'http://www.e-codices.unifr.ch/loris/cma/cma-1955-74/cma-1955-74_000b.jp2/info.json';
+//var iiifUrl = 'http://www.e-codices.unifr.ch/loris/cma/cma-1955-74/cma-1955-74_000b.jp2/info.json';
 
-var layer1 = L.tileLayer.iiif(iiifUrl, {
-    fitBounds: false,
+//var layer1 = L.tileLayer.iiif(iiifUrl, {
+ //   fitBounds: false,
+//  }
+//).addTo(map);
+
+//var layer2 = L.tileLayer.iiif(iiifUrl, {
+ //   quality: 'gray'
+ // }
+//).addTo(map);
+
+
+use this method to layer two separate image requestsinfo.json
+var layer1 = L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_natural_color_panorama_WIP2/info.json', {
+   fitBounds: false,
   }
-).addTo(map);
+ ).addTo(map);
 
-var layer2 = L.tileLayer.iiif(iiifUrl, {
-    quality: 'gray'
-  }
-).addTo(map);
-
-
-// use this method to layer two separate image requestsinfo.json
-// var layer1 = L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_natural_color_panorama_WIP2/info.json', {
-//     fitBounds: false,
-//   }
-// ).addTo(map);
-
-// var layer2 = L.tileLayer.iiif(
-//   'https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_combined_processes_WIP2_panorama_with_blue_water/info.json'
-// ).addTo(map);
+var layer2 = L.tileLayer.iiif(
+'https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_combined_processes_WIP2_panorama_with_blue_water/info.json'
+ ).addTo(map);
 
 L.control.sideBySide(layer1, layer2).addTo(map);
